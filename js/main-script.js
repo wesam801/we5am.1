3e5cc973833dfd35211fdc8d6ddfe49dd3dbcd50
wesam-shaker <90046091+wesam801@users.noreply.github.com>
2024-06-25 15:16:40 +0300

    Update main-script.js

--- js/main-script.js
+++ js/main-script.js
@@ -12,47 +12,22 @@ document.getElementById('submitBtn').addEventListener('click', function(){
     var textGm = document.getElementById('text-gm').value;
     var LengthSquare = tol * tol 
     var Bloc = gm / LengthSquare
+    document.getElementById('text-name').innerHTML = `الاسم : ${name}`;
+    document.getElementById('text-age').innerHTML = `العمر : ${age}`;
+    document.getElementById('text-tol').innerHTML = `الطول : ${tol} متر  `;
+    document.getElementById('text-gm').innerHTML = `الوزن : ${gm}`;
+    document.getElementById('bloc').innerHTML = ` النسبة : ${Bloc}`;
     if (Bloc < 18) {
-        document.getElementById('text-name').innerHTML = `${name}`;
-        document.getElementById('text-age').innerHTML = `${age}`;
-        document.getElementById('text-tol').innerHTML = `${tol}`;
-        document.getElementById('text-gm').innerHTML = `${gm}`;
-        document.getElementById('bloc').innerHTML = `${Bloc}`;
         document.getElementById('result').innerHTML = `نحافة`;
     }else if (Bloc >= 18 && Bloc <= 24.9) {
-        document.getElementById('text-name').innerHTML = `${name}`;
-        document.getElementById('text-age').innerHTML = `${age}`;
-        document.getElementById('text-tol').innerHTML = `${tol}`;
-        document.getElementById('text-gm').innerHTML = `${gm}`;
-        document.getElementById('bloc').innerHTML = `${Bloc}`;
         document.getElementById('result').innerHTML = `وزن مثالي`;
     }else if (Bloc >= 25 && Bloc <= 29.9) {
-        document.getElementById('text-name').innerHTML = `${name}`;
-        document.getElementById('text-age').innerHTML = `${age}`;
-        document.getElementById('text-tol').innerHTML = `${tol}`;
-        document.getElementById('text-gm').innerHTML = `${gm}`;
-        document.getElementById('bloc').innerHTML = `${Bloc}`;
         document.getElementById('result').innerHTML = `وزن زائد`;
     }else if (Bloc >= 30 && Bloc <= 34.9) {
-        document.getElementById('text-name').innerHTML = `${name}`;
-        document.getElementById('text-age').innerHTML = `${age}`;
-        document.getElementById('text-tol').innerHTML = `${tol}`;
-        document.getElementById('text-gm').innerHTML = `${gm}`;
-        document.getElementById('bloc').innerHTML = `${Bloc}`;
         document.getElementById('result').innerHTML = `سمنة درجة أولى`;
     }else if (Bloc >= 35 && Bloc <= 39.9) {
-        document.getElementById('text-name').innerHTML = `${name}`;
-        document.getElementById('text-age').innerHTML = `${age}`;
-        document.getElementById('text-tol').innerHTML = `${tol}`;
-        document.getElementById('text-gm').innerHTML = `${gm}`;
-        document.getElementById('bloc').innerHTML = `${Bloc}`;
         document.getElementById('result').innerHTML = `سمنة درجة ثانية`;
     }else if (Bloc > 40) {
-        document.getElementById('text-name').innerHTML = `${name}`;
-        document.getElementById('text-age').innerHTML = `${age}`;
-        document.getElementById('text-tol').innerHTML = `${tol}`;
-        document.getElementById('text-gm').innerHTML = `${gm}`;
-        document.getElementById('bloc').innerHTML = `${Bloc}`;
         document.getElementById('result').innerHTML = `سمنة خطيرة`;
     }
 })
